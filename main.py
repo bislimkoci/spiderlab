1690ff601807bdabd1a7512bf86a8cab54a820d4
bislimkoci <bislimkoci01@gmail.com>
2026-08-06 17:56:18 +0200

    Add erosien to futher remove noise

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ while camera.isOpened():
 
     retval, mask_thresh = cv2.threshold(fg_mask, 180, 255, cv2.THRESH_BINARY)
 
-    contours, hierarchy = cv2.findContours(mask_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
+
+    contours, hierarchy = cv2.findContours(mask_eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     frame_ct = cv2.drawContours(frame, contours, -1, (0, 255,0), 2)
 
