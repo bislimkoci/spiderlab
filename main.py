49b5e87f979bf25520855bb0a0ea6b00eae93413
bislimkoci <bislimkoci01@gmail.com>
2026-08-06 18:00:28 +0200

    Add filtering for the contrours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 
+MIN_CONTOUR_AREA = 400
 
 camera = cv2.VideoCapture(0)
 backsub = cv2.createBackgroundSubtractorMOG2()
@@ -21,7 +22,9 @@ while camera.isOpened():
 
     contours, hierarchy = cv2.findContours(mask_eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    frame_ct = cv2.drawContours(frame, contours, -1, (0, 255,0), 2)
+    large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > MIN_CONTOUR_AREA]
+
+    frame_ct = cv2.drawContours(frame, large_contours, -1, (0, 255,0), 2)
 
     cv2.imshow('frame', frame_ct)
 
