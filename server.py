71376c982cbbefc484707d2985351b9ab2074cef
bislimkoci <bislimkoci01@gmail.com>
2026-08-07 21:17:36 +0200

    Add so now can stream video from the server at /video

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 import cv2
+from fastapi.responses import StreamingResponse
 
 
 app = FastAPI()
 
-@app.get("/hello")
-async def hello():
-    return "Hello"
+def video_stream():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.get("/video")
+async def streaming_endpoint():
+    return StreamingResponse(video_stream(), media_type='multipart/x-mixed-replace; boundary=frame')
+
+
